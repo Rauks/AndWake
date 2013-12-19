1740b62127a8a15182adba08d0ec937c156d8392
Karl Woditsch <k.woditsch@gmail.com>
2013-12-19 13:38:39 +0100

    Prepare main menu

--- AndWake/gen/net/kirauks/andwake/R.java
+++ AndWake/gen/net/kirauks/andwake/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int menu_add_computer=0x7f080002;
+        public static final int menu_add_group=0x7f080003;
         public static final int pager=0x7f080000;
         public static final int textView1=0x7f080001;
     }
@@ -38,11 +39,12 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int computers_title=0x7f050004;
         public static final int favorites_title=0x7f050002;
         public static final int groups_title=0x7f050003;
+        public static final int menu_add_computer=0x7f050005;
+        public static final int menu_add_group=0x7f050001;
     }
     public static final class style {
         /** 
