d5d32340f8fbf76e6986759db139854738862895
Karl Woditsch <k.woditsch@gmail.com>
2013-12-21 21:23:37 +0100

    Add computer dialog form

--- AndWake/gen/net/kirauks/andwake/R.java
+++ AndWake/gen/net/kirauks/andwake/R.java
@@ -24,16 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_add_computer=0x7f080002;
-        public static final int menu_add_group=0x7f080003;
+        public static final int dialog_add_computer_address_field=0x7f08000b;
+        public static final int dialog_add_computer_address_label=0x7f08000a;
+        public static final int dialog_add_computer_mac_field_1=0x7f080004;
+        public static final int dialog_add_computer_mac_field_2=0x7f080005;
+        public static final int dialog_add_computer_mac_field_3=0x7f080006;
+        public static final int dialog_add_computer_mac_field_4=0x7f080007;
+        public static final int dialog_add_computer_mac_field_5=0x7f080008;
+        public static final int dialog_add_computer_mac_field_6=0x7f080009;
+        public static final int dialog_add_computer_mac_label=0x7f080003;
+        public static final int dialog_add_computer_name_field=0x7f080002;
+        public static final int dialog_add_computer_name_label=0x7f080001;
+        public static final int editText1=0x7f08000d;
+        public static final int menu_add_computer=0x7f08000e;
+        public static final int menu_add_group=0x7f08000f;
         public static final int pager=0x7f080000;
-        public static final int textView1=0x7f080001;
+        public static final int textView1=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_computers=0x7f030001;
-        public static final int fragment_favorites=0x7f030002;
-        public static final int fragment_groups=0x7f030003;
+        public static final int dialog_fragment_add_computer=0x7f030001;
+        public static final int fragment_computers=0x7f030002;
+        public static final int fragment_favorites=0x7f030003;
+        public static final int fragment_groups=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -41,6 +54,15 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int computers_title=0x7f050004;
+        public static final int dialog_add_computer_address=0x7f05000c;
+        public static final int dialog_add_computer_address_hint=0x7f05000d;
+        public static final int dialog_add_computer_mac=0x7f05000a;
+        public static final int dialog_add_computer_mac_hint=0x7f05000b;
+        public static final int dialog_add_computer_name=0x7f050008;
+        public static final int dialog_add_computer_name_hint=0x7f050009;
+        public static final int dialog_add_computer_port=0x7f05000e;
+        public static final int dialog_add_computer_port_defaut=0x7f05000f;
+        public static final int dialog_add_computer_port_hint=0x7f050010;
         public static final int dialog_cancel=0x7f050007;
         public static final int dialog_ok=0x7f050006;
         public static final int favorites_title=0x7f050002;
--- AndWake/src/net/kirauks/andwake/AddComputerDialogFragment.java
+++ AndWake/src/net/kirauks/andwake/AddComputerDialogFragment.java
@@ -5,12 +5,18 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class AddComputerDialogFragment extends DialogFragment{
 	@Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		
         return new AlertDialog.Builder(this.getActivity())
             .setTitle(R.string.menu_add_computer)
+            .setView(inflater.inflate(R.layout.dialog_fragment_add_computer, null))
             .setPositiveButton(R.string.dialog_ok,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
